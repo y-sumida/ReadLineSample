2b250bb823020bacae9a79ebd42af7ddaf05ecc5
Yuki Sumida <y.sumida.0201@gmail.com>
2017-06-20 23:14:21 +0900

    標準入力からスペース区切りの文字列を受け取る

---
+++ ReadLineSample/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ReadLineSample
+//
+//  Created by Yuki Sumida on 2017/06/20.
+//  Copyright © 2017年 Yuki Sumida. All rights reserved.
+//
+
+import Foundation
+
+let input = readLine()!
+print(input)
+
+let elements = input.components(separatedBy: " ")
+print(elements)
+
